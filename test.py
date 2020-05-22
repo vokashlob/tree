e3bad6d66df45f85c78e770611ed905a013b2aaf
Alexey Bolshakov <vokashlob@gmail.com>
2020-05-22 13:37:31 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,4 @@
+spis = ['d', 't', 'w', 'y', 'r', 's']
+print(len(spis))
+print(range(len(spis)))
+print((spis))
---
+++ tree.py
@@ -0,0 +1,40 @@
+# Define your "TreeNode" Python class below
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def __repr__(self):
+        return self.value
+
+    def add_child(self, child_node):
+        print("Adding " + child_node.value)
+        self.children.append(child_node)
+
+    def remove_child(self, child_node):
+        print("Removing " + child_node.value + " from " + self.value)
+        self.children = [child for child in self.children
+                         if child is not child_node]
+
+    def traverse(self):
+        print("Traversing...")
+        nodes_to_visit = [self]
+        while len(nodes_to_visit) > 0:
+            current_node = nodes_to_visit.pop()
+            print(current_node.value)
+            nodes_to_visit += current_node.children
+            #print(nodes_to_visit)
+
+
+root = TreeNode("CEO")
+first_child = TreeNode("Vice-President")
+second_child = TreeNode("Head of Marketing")
+third_child = TreeNode("Marketing Assistant")
+forth_child = TreeNode("zeeeeee!!!")
+
+root.add_child(first_child)
+root.add_child(second_child)
+second_child.add_child(third_child)
+first_child.add_child(forth_child)
+
+root.traverse()
